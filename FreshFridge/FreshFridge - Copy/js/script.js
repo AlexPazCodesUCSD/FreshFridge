51ce8818d5a3908489983d9755a2dd1cfbbdf467
maquilina27 <maquilina@ucsd.edu>
2024-05-06 18:58:37 -0700

    added folder

---
+++ FreshFridge/FreshFridge - Copy/js/script.js
@@ -0,0 +1,11 @@
+function linkedin(){
+  const link = window.open("https://www.linkedin.com/in/alex-paz-aa2135292/", '_blank');
+}
+
+function facebook(){
+  const link = window.open("https://www.facebook.com/alex.paz.9828456/", '_blank');
+}
+
+function email(){
+  const link = window.open("mailto:j6paz@ucsd.edu", '_blank');
+}
